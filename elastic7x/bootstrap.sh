a239cf464d99025670433389e203246c6ca368e7
Rodrigo Porto <rdrgporto@gmail.com>
2019-09-28 13:36:47 +0200

    Improved bootstrap.sh (6x & 7x) :art:
    Added PROVISION_FOLDER variable, apt instead of apt-get, removed DEBIAN_FRONTEND variable and prevent packages updates (apt mark hold)

--- elastic7x/bootstrap.sh
+++ elastic7x/bootstrap.sh
@@ -1,96 +1,115 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Functions
+
 DATE() {
   date '+%Y-%m-%d %H:%M:%S'
 }
 
 # Variables
+
+# Get IP
 IP=`ip -o addr show up primary scope global | while read -r num dev fam addr rest; do echo [$(DATE)] [Info] [System] ${addr%/*}; done`
 
 # Set package version
 VERSION="7.3.2"
 
-# Non-Interactive Installation
-export DEBIAN_FRONTEND=noninteractive
+# Set provision folder
+PROVISION_FOLDER="/vagrant"
+
+# Let's go
 
 # Update & Upgrade System
 echo "[$(DATE)] [Info] [System] Updating & Upgrading System..."
-apt-get update &> /dev/null
-apt-get upgrade -y &> /dev/null
+apt update &> /dev/null
+apt -y upgrade &> /dev/null
 
 # Install Java
 if [ $(dpkg-query -W -f='${Status}' openjdk-8-jdk 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   echo "[$(DATE)] [Info] [Java] Installing Java..."
-  add-apt-repository ppa:openjdk-r/ppa &> /dev/null
+  add-apt-repository -y ppa:openjdk-r/ppa &> /dev/null
   apt update &> /dev/null
-  apt-get -y install -y openjdk-8-jdk &> /dev/null
+  apt -y install openjdk-8-jdk &> /dev/null
 fi
 
 # Install Elastic Repository
 if ! grep -q "^deb .*7.x" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     echo "[$(DATE)] [Info] [System] Installing Elastic Repository..."
     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - &> /dev/null
-    apt-get -y install apt-transport-https &> /dev/null
+    apt -y install apt-transport-https &> /dev/null
     echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list &> /dev/null
-    apt-get update &> /dev/null
+    apt update &> /dev/null
 fi
 
 # Install Elasticsearch
 echo "[$(DATE)] [Info] [Elasticsearch] Installing Elasticsearch..."
-apt-get install -y elasticsearch=$VERSION &> /dev/null
+apt -y install elasticsearch=$VERSION &> /dev/null
 
 # Copy config, reload daemon and restart Elasticsearch
 echo "[$(DATE)] [Info] [Elasticsearch] Copy config, reload daemon and restart Elasticsearch..."
-cp -R /vagrant/elasticsearch/* /etc/elasticsearch/
-systemctl daemon-reload
-service elasticsearch restart
+cp -R $PROVISION_FOLDER/elasticsearch/* /etc/elasticsearch/ &> /dev/null
+systemctl daemon-reload &> /dev/null
+systemctl enable elasticsearch  &> /dev/null
+service elasticsearch restart  &> /dev/null
 
 # Install Kibana
 echo "[$(DATE)] [Info] [Kibana] Installing Kibana..."
-apt-get install -y kibana=$VERSION &> /dev/null
+apt -y install kibana=$VERSION &> /dev/null
 
 # Copy config, reload daemon and restart Kibana
 echo "[$(DATE)] [Info] [Kibana] Copy config, reload daemon and restart Kibana..."
-cp -R /vagrant/kibana/* /etc/kibana/
-systemctl daemon-reload
-service kibana restart
+cp -R $PROVISION_FOLDER/kibana/* /etc/kibana &> /dev/null
+systemctl daemon-reload &> /dev/null
+systemctl enable kibana &> /dev/null
+service kibana restart &> /dev/null
 
 # Install Logstash
 echo "[$(DATE)] [Info] [Logstash] Installing Logstash..."
-apt-get install -y logstash=1:$VERSION-1 &> /dev/null
-
+apt -y install logstash=1:$VERSION-1 &> /dev/null
+systemctl enable logstash &> /dev/null
+ 
 # Beats Family
 
 # Install Filebeat
 echo "[$(DATE)] [Info] [Filebeat] Installing Filebeat..."
-apt-get install -y filebeat=$VERSION &> /dev/null
+apt -y install filebeat=$VERSION &> /dev/null
+systemctl enable filebeat &> /dev/null
 
 # Install Packetbeat
 echo "[$(DATE)] [Info] [Packetbeat] Installing Packetbeat..."
-apt-get install -y libpcap0.8 &> /dev/null
-apt-get install -y packetbeat=$VERSION &> /dev/null
+apt -y install libpcap0.8 &> /dev/null
+apt -y install packetbeat=$VERSION &> /dev/null
+systemctl enable packetbeat &> /dev/null
 
 # Install Metricbeat
 echo "[$(DATE)] [Info] [Metricbeat] Installing Metricbeat..."
-apt-get install -y metricbeat=$VERSION &> /dev/null
+apt -y install metricbeat=$VERSION &> /dev/null
+systemctl enable metricbeat &> /dev/null
 
 # Install Heartbeat
 echo "[$(DATE)] [Info] [Heartbeat] Installing Heartbeat..."
-apt-get install -y heartbeat-elastic=$VERSION &> /dev/null
+apt -y install heartbeat-elastic=$VERSION &> /dev/null
+systemctl enable heartbeat-elastic &> /dev/null
 
 # Install Auditbeat
 echo "[$(DATE)] [Info] [Auditbeat] Installing Auditbeat..."
-apt-get install -y auditbeat=$VERSION &> /dev/null
+apt -y install auditbeat=$VERSION &> /dev/null
+systemctl enable auditbeat &> /dev/null
+
+# Tidying Up
 
 # Clean unneeded packages
 echo "[$(DATE)] [Info] [System] Cleaning unneeded packages..."
-apt-get autoremove -y &> /dev/null
+apt -y autoremove &> /dev/null
 
 # Update file search cache
 echo "[$(DATE)] [Info] [System] Updating file search cache..."
 updatedb &> /dev/null
+
+# Prevent package upgrade
+echo "[$(DATE)] [Info] [System] Prevent package upgrade..."
+apt-mark hold elasticsearch kibana logstash filebeat packetbeat metricbeat heartbeat-elastic auditbeat &> /dev/null
 
 # Show IPs
 echo "[$(DATE)] [Info] [System] IP Address on the machine..."
